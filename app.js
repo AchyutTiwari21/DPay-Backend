5f140f9df14e57cb10cfc8ffdae87ff4dc5db906
AchyutTiwari21 <achyut.s.tiwari@gmail.com>
2025-09-20 13:02:14 +0530

    Update cors origin

--- app.js
+++ app.js
@@ -6,10 +6,7 @@ import rateLimit from "express-rate-limit";
 const app = express();
 
 app.use(cors({
-    origin: [
-      process.env.CORS_ORIGIN,
-      process.env.DEV_ORIGIN
-    ],
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
